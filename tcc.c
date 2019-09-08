17dc50ae14a66089c1fbebc1bd8a7ebf6f732b18
tomoyukim <tomoyukim44@gmail.com>
2019-09-08 21:43:28 +0900

    add unary

--- tcc.c
+++ tcc.c
@@ -160,6 +160,18 @@ Node *new_node_num(int val) {
 }
 
 Node *expr();
+Node *primary();
+
+// unary = ("+" | "-")? primary
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    } else if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    } else {
+        return primary();
+    }
+}
 
 // primary = "(" expr ")" | num
 Node *primary() {
@@ -175,13 +187,13 @@ Node *primary() {
 
 // mul = primary ("*" primary | "/" primary)*
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   for (;;) {
     if (consume('*')) {
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     } else if (consume('/')) {
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     } else {
       return node;
     }
